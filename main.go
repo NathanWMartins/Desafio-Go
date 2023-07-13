ba2aa3c1a795930e056febb7d8dad4a664ae93ff
NathanWMartins <nathanwmartins@gmail.com>
2023-07-13 11:47:59 -0300

    go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import(
 
 func main(){
 	http.HandleFunc("/", func(rw http.ResponseWritter, r *http.Request){
-		fmt.Fprintf(rw,"Full Cycle")
+		fmt.Fprintf(rw,"Full Cycle Rcoks")
 	})
 
 	log.Fatal(http.listenAndServe(":8080", nil))
